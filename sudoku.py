d099b630c937eb1066c20bd64489f9a897b05fe6
Kirin Tang <ktang91@my.bcit.ca>
2023-02-26 22:48:21 -0800

    try to use a canvas inside subframe

--- sudoku.py
+++ sudoku.py
@@ -36,12 +36,27 @@ def change_colour(colour):
 
 def draw_sub_grid(row_num, col_num, sub_row_num, sub_col_num, bgcolour):
     frame = Frame(main_frame)
-    for i in range(sub_row_num):
-        for j in range(sub_col_num):
-            label = Label(frame, width=2, text=col_num+j+1,
-                          bg=bgcolour, justify="center", borderwidth=1, relief="solid")
-            label.grid(row=i+1, column=j+1,
-                       sticky="nsew", ipady=2)
+
+    # segment: use a canvas inside subframe
+    width = 80
+    height = 80
+    canvas = Canvas(frame, height=height, width=width, bg=bgcolour, bd=0, highlightthickness=0)
+    canvas.pack(fill=BOTH, expand=False)
+
+    for i in range(0, sub_row_num):
+        for j in range(0, sub_col_num):
+            x = i * width/sub_row_num
+            y = j * height/sub_col_num
+            canvas.create_rectangle(x, y, x + width/sub_row_num, y + height/sub_col_num, outline='black')
+            canvas.create_text(x + width/sub_row_num/2, y + height/sub_col_num/2, text=f"{int(col_num+y+1)}", font=(None, f'{width//sub_row_num//2}'))
+    # end of segment
+
+    # for i in range(sub_row_num):
+    #     for j in range(sub_col_num):
+    #         label = Label(frame, width=2, text=col_num+j+1,
+    #                       bg=bgcolour, justify="center", borderwidth=1, relief="solid")
+    #         label.grid(row=i+1, column=j+1,
+    #                    sticky="nsew", ipady=2)
     frame.grid(row=row_num+1, column=col_num+1)
 
 
